8ea35419179a7bbb4ac8d7b5676f511ad8f77575
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-06-28 16:48:31 -0400

    Added input task name functionality.

--- obj/Debug/net10.0/ToDoApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afebd7b87455688adbfbd280c334f75b643851e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb53618647fbc26aef5c33f6b8b22d8a3b2158ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
