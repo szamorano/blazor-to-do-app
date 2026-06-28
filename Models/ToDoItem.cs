afebd7b87455688adbfbd280c334f75b643851e9
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-06-28 15:36:00 -0400

    Initial commit ToDo app

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
