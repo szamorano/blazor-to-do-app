beac5bed6b31dee3ae8d79a7e297c8edfd14c5ac
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-19 13:02:20 -0400

    Use GitHub Actions.

--- obj/Debug/net10.0/ToDoApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4d5ec73641b2eace32d568c01325766440a351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07aa6d45fffe81cc245115ce770cbbcc26e36124")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
